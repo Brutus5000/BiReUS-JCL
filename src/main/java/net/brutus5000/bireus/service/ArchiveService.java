67be226a35852cd86609f5efb87f57026cd906f9
Christian Schmidt <c-schmidt@gmx.eu>
2017-05-08 20:54:12 +0200

    used ZipArchiveOutputStream instead of ZipOutputStream

--- src/main/java/net/brutus5000/bireus/service/ArchiveService.java
+++ src/main/java/net/brutus5000/bireus/service/ArchiveService.java
@@ -3,7 +3,9 @@ package net.brutus5000.bireus.service;
 import org.apache.commons.compress.archivers.ArchiveEntry;
 import org.apache.commons.compress.archivers.ArchiveInputStream;
 import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
+import org.apache.commons.compress.archivers.zip.ZipArchiveEntry;
 import org.apache.commons.compress.archivers.zip.ZipArchiveInputStream;
+import org.apache.commons.compress.archivers.zip.ZipArchiveOutputStream;
 import org.apache.commons.compress.compressors.xz.XZCompressorInputStream;
 import org.apache.commons.io.IOUtils;
 
@@ -14,8 +16,6 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Path;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipOutputStream;
 
 @Slf4j
 public class ArchiveService {
@@ -78,18 +78,18 @@ public class ArchiveService {
     }
 
     public static void compressFolderToZip(Path sourceDir, Path targetFile) throws IOException {
-        try (ZipOutputStream zipFile = new ZipOutputStream(new FileOutputStream(targetFile.toFile()))) {
+        try (ZipArchiveOutputStream zipFile = new ZipArchiveOutputStream(new FileOutputStream(targetFile.toFile()))) {
             compressDirectoryToZipfile(sourceDir, sourceDir, zipFile);
         }
     }
 
-    private static void compressDirectoryToZipfile(Path rootDir, Path sourceDir, ZipOutputStream out) throws IOException {
+    private static void compressDirectoryToZipfile(Path rootDir, Path sourceDir, ZipArchiveOutputStream out) throws IOException {
         for (File file : sourceDir.toFile().listFiles()) {
             if (file.isDirectory()) {
                 compressDirectoryToZipfile(rootDir, sourceDir.resolve(file.toPath()), out);
             } else {
-                ZipEntry entry = new ZipEntry(rootDir.relativize(file.toPath()).toString());
-                out.putNextEntry(entry);
+                ZipArchiveEntry entry = new ZipArchiveEntry(rootDir.relativize(file.toPath()).toString());
+                out.putArchiveEntry(entry);
 
                 try (FileInputStream in = new FileInputStream(sourceDir.resolve(file.toPath()).toFile())) {
                     IOUtils.copy(in, out);
