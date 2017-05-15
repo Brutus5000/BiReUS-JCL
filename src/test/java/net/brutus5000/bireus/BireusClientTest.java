7102de255160ca31d16b354efd13f6939f4432b1
Christian Schmidt <c-schmidt@gmx.eu>
2017-05-15 22:08:22 +0200

    added some corner-case tests

--- src/test/java/net/brutus5000/bireus/BireusClientTest.java
+++ src/test/java/net/brutus5000/bireus/BireusClientTest.java
@@ -1,11 +1,10 @@
 package net.brutus5000.bireus;
 
 
-import static junit.framework.TestCase.assertFalse;
-import static junit.framework.TestCase.assertTrue;
-import static net.brutus5000.bireus.TestUtil.assertFileEquals;
-import static net.brutus5000.bireus.TestUtil.assertZipFileEquals;
-
+import com.fasterxml.jackson.databind.ObjectMapper;
+import net.brutus5000.bireus.data.Repository;
+import net.brutus5000.bireus.mocks.DownloadServiceMock;
+import net.brutus5000.bireus.service.NotificationService;
 import org.apache.commons.io.FileUtils;
 import org.junit.After;
 import org.junit.Before;
@@ -14,11 +13,6 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import net.brutus5000.bireus.data.Repository;
-import net.brutus5000.bireus.mocks.DownloadServiceMock;
-import net.brutus5000.bireus.service.ArchiveService;
-import net.brutus5000.bireus.service.NotificationService;
-
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
@@ -26,11 +20,11 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.text.MessageFormat;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
+
+import static junit.framework.TestCase.assertFalse;
+import static junit.framework.TestCase.assertTrue;
+import static net.brutus5000.bireus.TestUtil.assertFileEquals;
+import static net.brutus5000.bireus.TestUtil.assertZipFileEquals;
 
 @RunWith(MockitoJUnitRunner.class)
 public class BireusClientTest {
@@ -42,7 +36,6 @@ public class BireusClientTest {
     Path clientRepositoryPath;
     Path firstVersionPath;
     Path latestVersionPath;
-
 
     @Before
     public void setUp() throws Exception {
@@ -89,6 +82,29 @@ public class BireusClientTest {
         assertFileEquals(firstVersionPath, clientRepositoryPath, "unchanged.txt");
         assertZipFileEquals(firstVersionPath, clientRepositoryPath, Paths.get("zip_sub", "changed-subfolder.test"));
         assertZipFileEquals(firstVersionPath, clientRepositoryPath, "changed.zip");
+    }
+
+    @Test
+    public void testSecondCheckoutWitPatchAlreadyOnDisk() throws Exception {
+        testCheckoutV1();
+
+        // make patch "already on disk"
+        Files.copy(
+                TestPreparator.getServerRepositoryPath()
+                        .resolve(Repository.BIREUS_PATCHES_SUBFOLDER)
+                        .resolve(MessageFormat.format(Repository.BIREUS_PATCH_FILE_PATTERN, "v1", "v2")),
+                clientRepositoryPath
+                        .resolve(Repository.BIREUS_INTERAL_FOLDER)
+                        .resolve(Repository.BIREUS_PATCHES_SUBFOLDER)
+                        .resolve(MessageFormat.format(Repository.BIREUS_PATCH_FILE_PATTERN, "v1", "v2")));
+
+        instance.checkoutVersion("v2");
+
+        assertFileEquals(latestVersionPath, clientRepositoryPath, Paths.get("new_folder", "new_file.txt"));
+        assertFileEquals(latestVersionPath, clientRepositoryPath, "changed.txt");
+        assertFileEquals(latestVersionPath, clientRepositoryPath, "unchanged.txt");
+        assertZipFileEquals(latestVersionPath, clientRepositoryPath, Paths.get("zip_sub", "changed-subfolder.test"));
+        assertZipFileEquals(latestVersionPath, clientRepositoryPath, "changed.zip");
     }
 
     @Test(expected = BireusException.class)
@@ -162,7 +178,7 @@ public class BireusClientTest {
         });
 
         downloadService.addDownloadAction((url, path) -> {
-            if(!url.toString().endsWith("/changed.txt"))
+            if (!url.toString().endsWith("/changed.txt"))
                 throw new AssertionError("Download URL not correct");
 
             Files.copy(TestPreparator.getServerRepositoryPath().resolve("v2/changed.txt"), path);
@@ -189,7 +205,7 @@ public class BireusClientTest {
         clientRepositoryPath = TestPreparator.prepareDownloadForLatestClientRepository(downloadService);
 
         downloadService.addReadAction(url -> Files.readAllBytes(TestPreparator.getServerRepositoryPath().resolve(Repository.BIREUS_INFO_FILE)));
-        downloadService.addDownloadAction((url, path) ->  {
+        downloadService.addDownloadAction((url, path) -> {
             throw new IOException("i.e. 404 not found");
         });
 
@@ -200,4 +216,16 @@ public class BireusClientTest {
         instance.checkoutVersion("non-existent-version");
     }
 
+    @Test(expected = BireusException.class)
+    public void testProtocolException() throws Exception {
+        clientRepositoryPath = TestPreparator.generateTemporaryClientRepositoryV1();
+
+        File infoFile = clientRepositoryPath.resolve(Repository.BIREUS_INTERAL_FOLDER).resolve(Repository.BIREUS_INFO_FILE).toFile();
+        ObjectMapper objectMapper = new ObjectMapper();
+        Repository repository = objectMapper.readValue(infoFile, Repository.class);
+        repository.setProtocolVersion(999);
+        objectMapper.writeValue(infoFile, repository);
+
+        instance = BireusClient.getFromURL(new URL("http://someurl"), clientRepositoryPath, notificationService, downloadService);
+    }
 }
